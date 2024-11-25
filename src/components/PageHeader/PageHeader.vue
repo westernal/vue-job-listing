0dad8240eb6c3a8c910be1634c6a53d41a9df7ff
ali navidi <ali_navidi24@yahoo.com>
2024-11-25 18:04:42 +0330

    Header added

---
+++ src/components/PageHeader/PageHeader.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <nav class="flex gap-5 align-middle justify-center">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">Jobs</RouterLink>
+      <RouterLink to="/about">Add Job</RouterLink>
+    </nav>
+    <RouterView />
+  </header>
+</template>
